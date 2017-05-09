c34a23a3a9191c739ce6ce89459fcac10850ff7f
key <lalala6630@126.com>
2017-05-09 13:31:19 +0800

    11

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,10 +9,7 @@ function resolve (dir) {
 }
 
 module.exports = {
-  entry: {
-    app: './src/main.js',
-    babelPolyfill:'babel-polyfill',
-  },
+  entry: ["babel-polyfill", "./src/main.js"],
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
