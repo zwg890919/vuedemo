b5c0ebd8b80c84c569897c6f8e4b8052ee1baf66
key <lalala6630@126.com>
2017-05-09 13:26:24 +0800

    1111

--- src/router/index.js
+++ src/router/index.js
@@ -106,5 +106,21 @@ export default new Router({
 				}]
 			}]
 		}
-	]
+	],
+    mode: 'history',
+    scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            const position = {}
+            if (to.hash) {
+            position.selector = to.hash
+            }
+            if (to.matched.some(m => m.meta.scrollToTop)) {
+            position.x = 0
+            position.y = 0
+            }
+            return position
+        }
+    }
 })
