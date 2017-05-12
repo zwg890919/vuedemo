c3322d226acc7de74c39ac52a330f7a834a37c43
lingjm <pecokael@163.com>
2017-05-12 09:33:38 +0800

    update

--- src/views/example/ui/fonts.vue
+++ src/views/example/ui/fonts.vue
@@ -1,77 +1,77 @@
 <template>
 	<div class="jyc-wrap">
         <h2>Fonts</h2>
-    	<Card>
-    		<div class="line"></div>
-			<Row :gutter="16">
-                <Col span="12">
-                    <div class="comp-box">
-                    	<Icon type="ionic"></Icon>
-                        <Icon type="arrow-up-a"></Icon>
-                        <Icon type="arrow-right-a"></Icon>
-                        <Icon type="arrow-down-a"></Icon>
-                        <Icon type="arrow-left-a"></Icon>
-                        <Icon type="arrow-up-b"></Icon>
-                        <Icon type="arrow-right-b"></Icon>
-                        <Icon type="arrow-down-b"></Icon>
-                        <Icon type="arrow-left-b"></Icon>
-                        <Icon type="arrow-up-c"></Icon>
-                        <Icon type="arrow-right-c"></Icon>
-                        <Icon type="arrow-down-c"></Icon>
-                        <Icon type="arrow-left-c"></Icon>
-                        <Icon type="arrow-return-right"></Icon>
-                        <Icon type="arrow-return-left"></Icon>
-                        <Icon type="arrow-swap"></Icon>
-                        <Icon type="arrow-shrink"></Icon>
-                        <Icon type="arrow-expand"></Icon>
-                        <Icon type="arrow-move"></Icon>
-                        <Icon type="arrow-resize"></Icon>
-                        <Icon type="chevron-up"></Icon>
-                        <Icon type="chevron-right"></Icon>
-                        <Icon type="chevron-down"></Icon>
-                        <Icon type="chevron-left"></Icon>
-                        <Icon type="navicon-round"></Icon>
-                        <Icon type="navicon"></Icon>
-                        <Icon type="drag"></Icon>
-                        <Icon type="log-in"></Icon>
-                        <Icon type="log-out"></Icon>
-                        <Icon type="checkmark-round"></Icon>
-                        <Icon type="checkmark"></Icon>
-                        <Icon type="checkmark-circled"></Icon>
-                        <Icon type="close-round"></Icon>
-                        <Icon type="close"></Icon>
-                        <Icon type="close-circled"></Icon>
-                        <Icon type="plus-round"></Icon>
-                        <Icon type="plus"></Icon>
-                        <Icon type="plus-circled"></Icon>
-                        <Icon type="minus-round"></Icon>
-                        <Icon type="minus"></Icon>
-                        <Icon type="minus-circled"></Icon>
-                        <Icon type="information"></Icon>
-                        <Icon type="information-circled"></Icon>
-                        <Icon type="help"></Icon>
-                        <Icon type="help-circled"></Icon>
-                        <Icon type="backspace-outline"></Icon>
-                        <Icon type="backspace"></Icon>
-                        <Icon type="help-buoy"></Icon>
-                        <Icon type="alert"></Icon>
-                        <Icon type="alert-circled"></Icon>
-                        <Icon type="refresh"></Icon>
-                        <Icon type="loop"></Icon>
-                        <Icon type="shuffle"></Icon>
-                    </div>
-                </Col>
-                <Col span="12">
-                    <div class="comp-box">
-                        <a href="https://www.iviewui.com/components/icon">更多icon请点击此处访问官网</a>
-                    </div>
-                </Col>
-        	</Row>
-    	</Card>
+        <div class="icon-box">
+            <Row>
+                <Col  :sm="8" :md="6"><Icon type="ionic"></Icon>ionic</Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-up-a"></Icon>arrow-up-a</Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-right-a"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-down-a"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-left-a"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-up-b"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-right-b"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-down-b"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-up-c"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-right-c"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-down-c"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-left-c"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-return-right"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-return-left"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-swap"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-shrink"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-expand"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-move"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="arrow-resize"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="chevron-up"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="chevron-right"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="chevron-down"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="chevron-left"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="navicon-round"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="navicon"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="drag"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="log-in"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="log-out"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="checkmark-round"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="checkmark"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="checkmark-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="close-round"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="close"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="close-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="plus-round"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="plus"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="plus-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="minus-round"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="minus"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="minus-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="information"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="information-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="help"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="help-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="backspace-outline"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="backspace"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="help-buoy"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="alert"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="alert-circled"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="refresh"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="loop"></Icon></Col>
+                <Col  :sm="8" :md="6"><Icon type="shuffle"></Icon></Col>
+         <!--        <Col  :sm="8" :md="6"></Col>
+                <Col  :sm="8" :md="6"></Col>
+                <Col  :sm="8" :md="6"></Col>
+                <Col  :sm="8" :md="6"></Col>
+                <Col  :sm="8" :md="6"></Col>
+                <Col  :sm="8" :md="6"></Col> -->
+            </Row>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
 .ivu-icon{
-    font-size: 45px;
+    font-size: 14px;
+    margin-right: 10px;
+}
+.icon-box{
+    font-size:14px;
+    padding: 20px;
 }
 </style>
