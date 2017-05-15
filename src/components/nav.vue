582edf61923a1aa33127b73042272e90a572213c
zhengshang <519710938@qq.com>
2017-05-15 11:43:50 +0800

    ui修改

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,72 +1,72 @@
 <template>
-  <div class="layout-nav" :class="{'layout-fixed':headerFixed}">
-    <div class="layout-nav__header" :class="{'layout-indent':asideIndent,'layout-fixed':asidefixed||headerFixed}">
-      <a href="" class="navbar-brand">
-        <Icon type="ionic"></Icon>
-        <span v-show="!asideIndent">聚有财理财</span>
-      </a>
-    </div>
-    <div class="layout-nav__con" :class="{'layout-indent':asideIndent}">
-      <a class="nav-btn" @click="changeIndent">
-        <Icon type="navicon"></Icon>
-      </a>
-      <a class="nav-btn">
-        <Icon type="ios-person-outline"></Icon>
-      </a>
-      <div class="layout-nav-dropdown" :class="{active : dropdown}" @click="dropdown = !dropdown">
-        <a >
-          应用
-          <Icon type="arrow-down-b"></Icon>
-        </a>
-        <div class="dropdown-area" v-show="dropdown">
-          <div @click="changeMenu(userapp)" v-for="userapp in applist" :key="userapp.id">
-            <p>{{userapp.name}}</p>
-            <img :src="'../../static'+userapp.menuIconclass" width="100" height="100" alt="">
-          </div>
+    <div class="layout-nav" :class="{'layout-fixed':headerFixed}">
+        <div class="layout-nav__header" :class="{'layout-indent':asideIndent,'layout-fixed':asidefixed||headerFixed}">
+            <a href="" class="navbar-brand">
+                <Icon type="ionic"></Icon>
+                <span v-show="!asideIndent">聚有财理财</span>
+            </a>
         </div>
-      </div>
-      <div class="navbar-right">
-        <a class="nav-btn">
-          <Icon type="ios-bell-outline"></Icon>
-        </a>
-        <a class="nav-btn" @click="settingshow = !settingshow">
-          <Icon type="ios-cog"></Icon>
-        </a>
-      </div>
+        <div class="layout-nav__con" :class="{'layout-indent':asideIndent}">
+            <a class="nav-btn" @click="changeIndent">
+                <Icon type="ios-browsers-outline"></Icon>
+            </a>
+            <a class="nav-btn">
+                <Icon type="ios-person-outline"></Icon>
+            </a>
+            <div class="layout-nav-dropdown" :class="{active : dropdown}" @click="dropdown = !dropdown">
+                <a>
+                    应用
+                    <Icon type="arrow-down-b"></Icon>
+                </a>
+                <div class="dropdown-area" v-show="dropdown">
+                    <div @click="changeMenu(userapp)" v-for="userapp in applist" :key="userapp.id">
+                        <p>{{userapp.name}}</p>
+                        <img :src="'../../static'+userapp.menuIconclass" width="100" height="100" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-right">
+                <a class="nav-btn">
+                    <Icon type="ios-bell-outline"></Icon>
+                </a>
+                <a class="nav-btn" @click="settingshow = !settingshow">
+                    <Icon type="ios-gear-outline"></Icon>
+                </a>
+            </div>
+        </div>
+        <setting :settingshow="settingshow"></setting>
     </div>
-    <setting :settingshow="settingshow"></setting>
-  </div>
 </template>
 <script>
 import setting from "./setting"
 import { mapActions } from 'vuex'
 
 export default {
-  data() {
-    return {
-      dropdown: false,
-      settingshow: false,
-    }
-  },
-  props: ["asideIndent", 'headerFixed', 'asidefixed'],
-  computed: {
-    applist: v => v.$store.state.appMenu.applist
-  },
-  methods: {
-    changeIndent() {
-      this.$store.commit("changeSetting", 4)
+    data() {
+        return {
+            dropdown: false,
+            settingshow: false,
+        }
     },
-    ...mapActions([
-      'changeMenu'
-    ]),
-  },
-  components: {
-    setting
-  }
+    props: ["asideIndent", 'headerFixed', 'asidefixed'],
+    computed: {
+        applist: v => v.$store.state.appMenu.applist
+    },
+    methods: {
+        changeIndent() {
+            this.$store.commit("changeSetting", 4)
+        },
+        ...mapActions([
+            'changeMenu'
+        ]),
+    },
+    components: {
+        setting
+    }
 }
 </script>
 <style lang="scss">
 .navbar-right {
-  float: right;
+    float: right;
 }
 </style>
--- src/views/example/ui/toaster.vue
+++ src/views/example/ui/toaster.vue
@@ -1,89 +1,154 @@
 <template>
-	<div class="jyc-wrap">
+    <div class="jyc-wrap">
         <h2>Toaster</h2>
-    	<Card>
-    		<div class="line"></div>
-			<Row :gutter="16">
+        <Card>
+            <Row :gutter="16">
                 <Col span="12">
-                    <div class="comp-box">
-                    	<Button type="primary" @click="open(false)">打开提醒</Button>
-                        <Button @click="open(true)">打开提醒（仅标题）</Button>
-                    </div>
+                <div class="jyc-wrap-space">
+                    <Button type="primary" @click="open(false)">打开提醒</Button>
+                    <Button @click="open(true)">打开提醒（仅标题）</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <div>带描述信息</div>
+                    <Button @click="info(false)">消息</Button>
+                    <Button @click="success(false)">成功</Button>
+                    <Button @click="warning(false)">警告</Button>
+                    <Button @click="error(false)">错误</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <div>仅标题</div>
+                    <Button @click="info(true)">消息</Button>
+                    <Button @click="success(true)">成功</Button>
+                    <Button @click="warning(true)">警告</Button>
+                    <Button @click="error(true)">错误</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <div>不会自动关闭</div>
+                    <Button type="primary" @click="time">打开提醒</Button>
+                </div>
                 </Col>
                 <Col span="12">
-                    <div class="comp-box">
-                        <p>带描述信息</p>
-                        <Button @click="info(false)">消息</Button>
-                        <Button @click="success(false)">成功</Button>
-                        <Button @click="warning(false)">警告</Button>
-                        <Button @click="error(false)">错误</Button>
-                        <p>仅标题</p>
-                        <Button @click="info(true)">消息</Button>
-                        <Button @click="success(true)">成功</Button>
-                        <Button @click="warning(true)">警告</Button>
-                        <Button @click="error(true)">错误</Button>
-                    </div>
+                <div class="jyc-wrap-space">
+                    <Button type="primary" @click="messageInfo">显示普通提醒</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <Button @click="messageSuccess">显示成功提示</Button>
+                    <Button @click="messageWarning">显示警告提示</Button>
+                    <Button @click="messageError">显示错误提示</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <Button @click="messageLoading">显示加载中...</Button>
+                </div>
+                <div class="jyc-wrap-space">
+                    <Button type="success" @click="messageOther(1)">success</Button>
+                    <Button type="info" @click="messageOther(2)">info</Button>
+                    <Button @click="messageOther(3)">wait</Button>
+                    <Button type="error" @click="messageOther(4)">error</Button>
+                    <Button type="warning" @click="messageOther(5)">warning</Button>
+                </div>
                 </Col>
-        	</Row>
-    	</Card>
-    	<Card>
-    		<div class="line"></div>
-			<Row :gutter="16">
-                <Col span="12">
-                    <div class="comp-box">
-                    	<Button type="primary" @click="time">打开提醒</Button>
-                    </div>
-                </Col>
-                <Col span="12">
-                    <div class="comp-box">
-                        
-                    </div>
-                </Col>
-        	</Row>
-    	</Card>
+            </Row>
+        </Card>
     </div>
 </template>
 
 <script>
-    export default {
-        methods: {
-            open (nodesc) {
-                this.$Notice.open({
-                    title: '这是通知标题',
-                    desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-                });
-            },
-            info (nodesc) {
-                this.$Notice.info({
-                    title: '这是通知标题',
-                    desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-                });
-            },
-            success (nodesc) {
-                this.$Notice.success({
-                    title: '这是通知标题',
-                    desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-                });
-            },
-            warning (nodesc) {
-                this.$Notice.warning({
-                    title: '这是通知标题',
-                    desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-                });
-            },
-            error (nodesc) {
-                this.$Notice.error({
-                    title: '这是通知标题',
-                    desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
-                });
-            },
-            time () {
-                this.$Notice.open({
-                    title: '这是通知标题',
-                    desc: '这条通知不会自动关闭，需要点击关闭按钮才可以关闭。',
-                    duration: 0
-                });
+export default {
+    methods: {
+        open(nodesc) {
+            this.$Notice.open({
+                title: '这是通知标题',
+                desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
+            });
+        },
+        info(nodesc) {
+            this.$Notice.info({
+                title: '这是通知标题',
+                desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
+            });
+        },
+        success(nodesc) {
+            this.$Notice.success({
+                title: '这是通知标题',
+                desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
+            });
+        },
+        warning(nodesc) {
+            this.$Notice.warning({
+                title: '这是通知标题',
+                desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
+            });
+        },
+        error(nodesc) {
+            this.$Notice.error({
+                title: '这是通知标题',
+                desc: nodesc ? '' : '这里是通知描述这里,是通知描述这里是通知描述这里,是通知描述这里,是通知描述这里是通知描述这里是通知描述'
+            });
+        },
+        time() {
+            this.$Notice.open({
+                title: '这是通知标题',
+                desc: '这条通知不会自动关闭，需要点击关闭按钮才可以关闭。',
+                duration: 0
+            });
+        },
+        messageInfo() {
+            this.$Message.info('这是一条普通的提醒');
+        },
+        messageSuccess() {
+            this.$Message.success('这是一条成功的提示');
+        },
+        messageWarning() {
+            this.$Message.warning('这是一条警告的提示');
+        },
+        messageError() {
+            this.$Message.error('对方不想说话，并且向你抛出了一个异常');
+        },
+        messageLoading() {
+            const msg = this.$Message.loading('正在加载中...', 0);
+            setTimeout(msg, 3000);
+        },
+        messageOther(type) {
+            switch (type) {
+                case 1: 
+                    this.$totast.success({
+                        title: 'title',
+                        message: 'message'
+                    });
+                    break;
+                case 2:
+                    this.$totast.info({
+                        title: 'title',
+                        message: 'message'
+                    });
+                    break;
+                case 3: 
+                    this.$totast.wait({
+                        title: 'title',
+                        message: 'message'
+                    });
+                    break;
+                case 4: 
+                    this.$totast.error({
+                        title: 'title',
+                        message: 'message'
+                    });
+                    break;
+                case 5: 
+                    this.$totast.warning({
+                        title: 'title',
+                        message: 'message'
+                    });
+                    break;
+                default:
+                    break;
             }
         }
     }
+}
 </script>
+<style lang="scss" scoped>
+.jyc-wrap-space {
+    margin-bottom: 20px;
+}
+</style>
